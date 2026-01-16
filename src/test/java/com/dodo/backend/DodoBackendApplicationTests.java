102a3d4d20267740a257a412ac0342bbbaca6fcb
Baek HyeonBin <81628455+WhiteBin-bin@users.noreply.github.com>
2026-01-16 21:52:27 +0900

    ⚙️ Chore: 프로젝트 기본 세팅 (#2)

---
+++ src/test/java/com/dodo/backend/DodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
